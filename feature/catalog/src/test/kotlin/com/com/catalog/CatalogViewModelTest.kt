79e83d4fd7b4c4ba00b30b2f04e89e9a65e20d96
Julian Verjan <julian.verjan@zenjob.com>
2021-03-08 18:26:04 +0100

    Fix on view model unit test is done

--- feature/catalog/src/test/kotlin/com/com/catalog/CatalogViewModelTest.kt
+++ feature/catalog/src/test/kotlin/com/com/catalog/CatalogViewModelTest.kt
@@ -3,11 +3,12 @@ package com.com.catalog
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import com.products.catalog.catalog.CatalogViewModel
+import com.products.catalog.model.UIState
 import com.products.data.usecase.FetchCatalogUseCase
-import com.products.network.model.NetworkResponse
-import com.products.network.model.reponse.CategoryNetworkResponse
-import com.products.network.model.reponse.ProductNetworkResponse
-import com.products.network.model.reponse.SalePriceNetworkResponse
+import com.products.model.Category
+import com.products.model.Product
+import com.products.model.SalePrice
+import com.products.network.model.mapper.RepositoryResult
 import com.products.network.service.CatalogService
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNotNull
@@ -32,15 +33,15 @@ class CatalogViewModelTest {
     private var catalogViewModel = CatalogViewModel(fetchCatalogUseCase)
     private lateinit var catalogService: CatalogService
 
-    private val categoryNetworkList = listOf(
-        CategoryNetworkResponse(
+    private val categoryList = listOf(
+        Category(
             "424", "food", "test",
             listOf(
-                ProductNetworkResponse(
+                Product(
                     "343", "424",
                     "bread", "dfgdf.png",
                     "testProduct",
-                    SalePriceNetworkResponse("23", "EUR")
+                    SalePrice("23", "EUR")
                 )
             )
         )
@@ -63,8 +64,9 @@ class CatalogViewModelTest {
     @Test
     fun fetchCatalogSuccessfully() {
         testScope.launch {
-            whenever(catalogService.fetchCategories())
-                .thenReturn(NetworkResponse.Success(categoryNetworkList))
+            whenever(fetchCatalogUseCase.fetchCatalog())
+                .thenReturn(RepositoryResult.Success(categoryList))
+
             catalogViewModel.fetchCatalog()
             catalogViewModel.viewStateFlow.collect {
                 assertNotNull(it.result)
@@ -73,4 +75,47 @@ class CatalogViewModelTest {
             }
         }
     }
+
+    @Test
+    fun fetchCatalogSuccessfullyWithEmptyView() {
+        testScope.launch {
+            whenever(fetchCatalogUseCase.fetchCatalog())
+                .thenReturn(RepositoryResult.Success(emptyList()))
+
+            catalogViewModel.fetchCatalog()
+            catalogViewModel.viewStateFlow.collect {
+                assertNotNull(it.result)
+                assertEquals(it.result.size, 0)
+            }
+        }
+    }
+
+    @Test
+    fun fetchCatalogWithError() {
+        testScope.launch {
+            whenever(fetchCatalogUseCase.fetchCatalog())
+                .thenReturn(RepositoryResult.Fail("error"))
+
+            catalogViewModel.fetchCatalog()
+            catalogViewModel.viewStateFlow.collect {
+                assertNotNull(it.result)
+                assertEquals(it.uiState, UIState.ERROR)
+            }
+        }
+    }
+
+    @Test
+    fun fetchCatalogWithApiError() {
+        testScope.launch {
+            whenever(fetchCatalogUseCase.fetchCatalog())
+                .thenReturn(RepositoryResult.Exception(Exception()))
+
+            catalogViewModel.fetchCatalog()
+            catalogViewModel.viewStateFlow.collect {
+                assertNotNull(it.result)
+                assertEquals(it.uiState, UIState.ERROR)
+            }
+        }
+    }
+
 }
